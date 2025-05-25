9fcbeaa659bb812627e19b3f1d8f023ce1ea1605
juliabush <j.elizabethbush@gmail.com>
2025-05-25 12:18:52 +0200

    Experementing with react components like writing hello world and displaying picture

---
+++ src/Favefood.jsx
@@ -0,0 +1,8 @@
+// import React from "react";
+import Brownie from "./assets/brownie.jpg";
+
+function Favefood() {
+  return <img src={Brownie} alt="Delicious brownie" />;
+}
+
+export default Favefood;
---
+++ src/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting() {
+  return <h1>&quot;Hello everybody&quot;</h1>;
+}
+
+export default Greeting;
